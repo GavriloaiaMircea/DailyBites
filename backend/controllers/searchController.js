d69482d95a5c6b04f3a1f7f8f9a5bbf0ca52555b
GavriloaiaMircea <rowan.mtg@yahoo.com>
2024-12-01 12:11:48 +0200

    Add food details endpoint and refactor searchFood function for improved API integration

--- backend/controllers/searchController.js
+++ backend/controllers/searchController.js
@@ -1,3 +1,5 @@
+const apiKey = process.env.USDA_API_KEY;
+
 export const searchFood = (req, res) => {
   const { query } = req.query;
 
@@ -5,10 +7,9 @@ export const searchFood = (req, res) => {
     return res.status(400).json({ error: "Query is required" });
   }
 
-  const apiKey = process.env.USDA_API_KEY;
-  const apiUrl = `${process.env.USDA_API_URL}/search?query=${encodeURIComponent(
-    query
-  )}&api_key=${apiKey}`;
+  const apiUrl = `${
+    process.env.USDA_API_URL
+  }/foods/search?query=${encodeURIComponent(query)}&api_key=${apiKey}`;
 
   fetch(apiUrl)
     .then((response) => response.json())
@@ -27,5 +28,56 @@ export const searchFood = (req, res) => {
     .catch((error) => {
       console.error("Error fetching data from USDA API:", error);
       res.status(500).json({ error: "Failed to fetch data" });
+    });
+};
+
+export const getFoodDetails = (req, res) => {
+  const { id } = req.params;
+
+  if (!id) {
+    return res.status(400).json({ error: "Food ID is required" });
+  }
+
+  const apiUrl = `${process.env.USDA_API_URL}/food/${encodeURIComponent(
+    id
+  )}?api_key=${apiKey}`;
+
+  fetch(apiUrl)
+    .then((response) => response.json())
+    .then((data) => {
+      if (!data.labelNutrients) {
+        return res
+          .status(404)
+          .json({ message: "No nutritional details found" });
+      }
+
+      const nutritionalInfo = {
+        name: data.description,
+        servingSize: `${data.servingSize} ${data.servingSizeUnit}`,
+        calories: data.labelNutrients.calories?.value || 0,
+        macronutrients: {
+          carbohydrates: data.labelNutrients.carbohydrates?.value || 0,
+          protein: data.labelNutrients.protein?.value || 0,
+          fat: data.labelNutrients.fat?.value || 0,
+          fiber: data.labelNutrients.fiber?.value || 0,
+          sugars: data.labelNutrients.sugars?.value || 0,
+        },
+        micronutrients: {
+          calcium: data.labelNutrients.calcium?.value || 0,
+          iron: data.labelNutrients.iron?.value || 0,
+          potassium: data.labelNutrients.potassium?.value || 0,
+        },
+        otherNutrients: {
+          saturatedFat: data.labelNutrients.saturatedFat?.value || 0,
+          cholesterol: data.labelNutrients.cholesterol?.value || 0,
+          sodium: data.labelNutrients.sodium?.value || 0,
+        },
+      };
+
+      res.json(nutritionalInfo);
+    })
+    .catch((error) => {
+      console.error("Error fetching data from USDA API:", error);
+      res.status(500).json({ error: "Failed to fetch product details" });
     });
 };
--- backend/routes/searchRoutes.js
+++ backend/routes/searchRoutes.js
@@ -1,8 +1,9 @@
 import express from "express";
-import { searchFood } from "../controllers/searchController.js";
+import { getFoodDetails, searchFood } from "../controllers/searchController.js";
 
 const router = express.Router();
 
 router.get("/search", searchFood);
+router.get("/details/:id", getFoodDetails);
 
 export default router;
